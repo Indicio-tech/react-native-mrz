abc328ab00ef201c2fc48252d9675370003f7b45
Kim Ebert <kim@indicio.tech>
2020-12-17 13:22:01 -0700

    Add models and read model
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- android/src/main/java/com/reactlibrary/MrzModule.java
+++ android/src/main/java/com/reactlibrary/MrzModule.java
@@ -5,6 +5,10 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MrzModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
@@ -21,6 +25,30 @@ public class MrzModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void sampleMethod(String stringArgument, int numberArgument, Promise promise) {
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                new InputStreamReader(reactContext.getAssets().open("models/ESC-v2.svm.model")));
+
+            // do reading, usually loop until end of file reading  
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+            //process line
+                promise.resolve(mLine);
+            }
+        } catch (IOException e) {
+            //log the exception
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+
         // TODO: Implement some actually useful functionality
         //callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
         String result = "Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument;
