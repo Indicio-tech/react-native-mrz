8e83d79ba9bdf171fb196e022980aad9fcf14cee
Kim Ebert <kim@indicio.tech>
2021-02-20 21:17:36 -0700

    Commit ios progress.
    Signed-off-by: Kim Ebert <kim@indicio.tech>

--- ios/Mrz.h
+++ ios/Mrz.h
@@ -1,5 +1,6 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTLog.h>
+#import <TesseractOCR/TesseractOCR.h>
 
 
 @interface Mrz : NSObject <RCTBridgeModule>
--- ios/Mrz.m
+++ ios/Mrz.m
@@ -8,6 +8,21 @@ RCT_EXPORT_METHOD(ocrFile:(NSString *)stringArgument resolver:(RCTPromiseResolve
 {
     RCTLogInfo(@"Here");
 
+    G8Tesseract *tesseract = [[G8Tesseract alloc] initWithLanguage:@"ocrb"];
+
+    RCTLogInfo(@"Here2");
+
+    // Specify the image Tesseract should recognize on
+    tesseract.image = [[UIImage imageNamed:@"image_sample.jpg"] g8_blackAndWhite];
+
+    // Start the recognition
+    [tesseract recognize];
+
+    // Retrieve the recognized text
+    RCTLogInfo(@"%@", [tesseract recognizedText]);
+
+    RCTLogInfo(@"Here3");
+
     // TODO: Implement some actually useful functionality
     resolve(@[[NSString stringWithFormat: @"stringArgument: %@", stringArgument]]);
 }
